8319b79c890d167e2a8553b2776a3b2088ae87e0
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-14 02:01:14 -0400

    install react-router-dom and set up routes for user login and regisatrtion

---
+++ src/assets/components/Registration.jsx
@@ -0,0 +1,9 @@
+function Registration() {
+  return (
+    <>
+      <h1>Registration Page</h1>
+    </>
+  );
+}
+
+export default Registration;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App.jsx';
+import Login from './assets/components/Login.jsx';
+import Registration from './assets/components/Registration.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/registration" element={<Registration />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
