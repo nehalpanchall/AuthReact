f484978fec169c5bdd732cd1e50fb418dd444bac
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-15 01:52:23 -0400

    define baseURL of backend server and default headers in a constructor of ApiClient class

--- service/apiClient.js
+++ service/apiClient.js
@@ -1,3 +1,9 @@
 class ApiClient {
-  constructor() {}
+  constructor() {
+    this.baseURL = 'http://localhost:60000/api/v1';
+    this.defaultHeaders = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    };
+  }
 }
