df4c237deb4aa10c04966931c6731e9582174253
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-15 01:34:11 -0400

    design user registration form with all required fields

--- src/assets/components/Registration.jsx
+++ src/assets/components/Registration.jsx
@@ -4,10 +4,50 @@ function Registration() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState('');
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
-  return <></>;
+  return (
+    <>
+      <form>
+        <label>Username: </label>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          autoFocus
+        />
+        <br />
+        <br />
+
+        <label>Email: </label>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <br />
+        <br />
+
+        <label>Password: </label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <br />
+        <br />
+
+        <button type="submit">{loading ? 'Please wait...' : 'Register'}</button>
+      </form>
+    </>
+  );
 }
 
 export default Registration;
