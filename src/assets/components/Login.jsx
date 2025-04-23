26c1b2ab7933605df6c4d6347602e2201eaf18a7
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-23 00:58:51 -0400

    fix: add missing onChange handlers to login form fields

--- src/assets/components/Login.jsx
+++ src/assets/components/Login.jsx
@@ -16,10 +16,23 @@ function Login() {
     <>
       <form onSubmit={submitHandle}>
         <label htmlFor="email">Email: </label>
-        <input type="text" name="email" id="email" value={email} /> <br />
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />{' '}
+        <br />
         <br />
         <label htmlFor="password">Password: </label>
-        <input type="password" name="password" id="password" value={password} />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <br /> <br />
         <button type="submit" disabled={loading}>
           {loading ? 'Please wait' : 'Login'}
