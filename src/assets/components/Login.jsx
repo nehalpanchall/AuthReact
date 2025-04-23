709d33ad41a7b98cc200a034f0b62b51a5088b92
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-23 00:41:58 -0400

    feat: design login form UI using JSX and declare state variables in Login component

--- src/assets/components/Login.jsx
+++ src/assets/components/Login.jsx
@@ -1,7 +1,26 @@
+import { use, useState } from 'react';
+
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const submitHandle = () => {};
+
   return (
     <>
-      <h1>Login Page</h1>
+      <form onSubmit={submitHandle}>
+        <label htmlFor="email">Email: </label>
+        <input type="text" name="email" id="email" value={email} /> <br />{' '}
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input type="password" name="password" id="password" value={password} />
+        <br /> <br />
+        <button type="submit" disabled={loading}>
+          {loading ? 'Please wait' : 'Login'}
+        </button>
+      </form>
     </>
   );
 }
