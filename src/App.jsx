525d91d65628cda8a24afc4f97e5d22ed1e55006
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-25 01:05:00 -0400

    feat: add links on home page to navigate users to login and register UI page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 function App() {
   return (
     <>
       <h1>React + Node Authentication</h1>
+      <Link to="/login">Login</Link> <br></br>
+      <Link to="/registration">Register</Link>
     </>
   );
 }
