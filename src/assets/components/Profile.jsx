e704210c67cc71211d49f6e14f8cd3e1781ae2ba
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-25 01:54:57 -0400

    invoke userProfile() method using singleton apiClient object and extract user data in Profile component

--- src/assets/components/Profile.jsx
+++ src/assets/components/Profile.jsx
@@ -1,4 +1,13 @@
+import apiClient from '../../../service/apiClient';
+
 const Profile = () => {
+  try {
+    const response = apiClient.userProfile();
+    response.then((result) => {
+      const userData = result.data;
+      const { _id, userName, email, role } = userData;
+    });
+  } catch (error) {}
   return (
     <>
       <h1>User Profile</h1>
